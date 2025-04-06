1aeb792bfd4d2d9ab4fc232274c314bbb12926b2
yanglvyou <yang376607092@gmail.com>
2025-04-06 17:59:27 +0800

    fix: bug

--- src/index.ts
+++ src/index.ts
@@ -299,7 +299,7 @@ const renameFiles: Record<string, string | undefined> = {
   _gitignore: ".gitignore",
 };
 
-const defaultTargetDir = "vite-project1";
+const defaultTargetDir = "vite-project";
 
 async function init() {
   const argTargetDir = argv._[0]
